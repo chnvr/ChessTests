d5039f58d181364cde3c81d2bd995a1cf5445ae0
chnvr <buzmakova.ulianna@gmail.com>
2026-06-20 05:31:56 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,37 +1,37 @@
-﻿using Xunit;
+using Xunit;
 using ChessExample;
 using System;
-namespace CheckChesPositionTest
+namespace CheckerChessPositionTest
 {
     public class Tests
     {
         [Theory]
-        [InLineData(1, 1, "A", "A1")]
-        [InLineData(8, 8, "H", "H8")]
-        [InLineData(4, 5, "D", "D8")]
+        [InlineData(1, 1, "A", "A1")]
+        [InlineData(8, 8, "H", "H8")]
+        [InlineData(4, 5, "D", "D5")]
         public void Test1(byte x, byte y, char expectedLetter, string expectedString)
         {
-            var pos = new CheckChesPositionTest(x, y);
-            Assert.True(isComplete);
-            Assert.NotNull(result);
-            Assert.Equal(expectedLetterY, pos.XLetter);
-            Assert.Equal(expectedLetterX, pos.ToString();
+            var pos = new CheckerBoardPosition(x, y);
+            Assert.True(x, pos X);
+            Assert.NotNull(y, pos Y);
+            Assert.Equal(expectedLetter, pos.XLetter);
+            Assert.Equal(expectedLetter, pos.ToString();
         }
         [Theory]
-        [InLineData(0, 1)]
-        [InLineData(9, 5)]
-        [InLineData(1, 0)]
+        [InlineData(0, 1)]
+        [InlineData(9, 5)]
+        [InlineData(1, 0)]
         public void Test2(byte x, byte y)
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => new CheckBoardPosition(x, y));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CheckerBoardPosition(x, y));
         }
         [Theory]
-        [InLineData("A1", 1, 1)]
-        [InLineData("H8", 8, 8)]
-        [InLineData("E4", 5, 4)]
+        [InlineData("A1", 1, 1)]
+        [InlineData("H8", 8, 8)]
+        [InlineData("E4", 5, 4)]
         public void Test3(string input, byte expectedX, byte expectedY)
         {
-            var pos = CheckChesPositionTest.Parse(input, null):
+            var pos = CheckerBoardPosition.Parse(input, null):
             Assert.NotNull(pos);
             Assert.Equal(expectedX, pos.X);
             Assert.Equal(expectedY, pos.Y);
@@ -39,20 +39,35 @@ namespace CheckChesPositionTest
 
         }
         [Theory]
-        [InLineData("A0")]
-        [InLineData("I5")]
-        [InLineData("")]
-        [InLineData(null)]
+        [InlineData("A0")]
+        [InlineData("I5")]
+        [InlineData("")]
+        [InlineData(null)]
         public void Test4(string input)
         {
-            Assert.Throws<FormatException>(() => new CheckBoardPosition.Parse(input, null));
+            Assert.Throws<FormatException>(() => new CheckerBoardPosition.Parse(input, null));
         }
         [Theory]
-        [InLineData("B2", 2, 2)]
-        [InLineData("C7", 3, 7)]
+        [InlineData("B2", 2, 2)]
+        [InlineData("C7", 3, 7)]
         public void Test5(string input, byte expectedX, byte expectedY)
         {
-            bool success = CheckChesPosition.TryParse(input, w);
+            bool success = CheckerBoardPosition.TryParse(input, null, out var pos);
+            Assert.True(success);
+            Assert.NotNull(pos);
+            Assert.Equal(expectedX, pos.X);
+            Assert.Equal(expectedY, pos.Y);
+        }
+        [Theory]
+        [InlineData("Z1")]
+        [InlineData("A")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Test6(string input)
+        {
+            bool sucess = CheckerBoardPosition.TryParse(input, null, out var pos);
+            Assert.True(success);
+            Assert.NotNull(pos);
         }
     }
 }
